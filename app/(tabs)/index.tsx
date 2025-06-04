ed84383273f7ca3bb5c84d9cd2f9471afd25b987
Bryan Juanacio <bryanjuanacio@Mac-mini-de-Bryan.local>
2025-06-03 20:04:22 -0500

    feat: start profile screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,15 @@
-import Button from "@/components/button";
 import ScreenWrapper from "@/components/screen-wrapper";
 import Typo from "@/components/typo";
-import { auth } from "@/config/firebase";
-import { colors } from "@/constants/theme";
 import { useAuth } from "@/context/auth-context";
-import { signOut } from "firebase/auth";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const Home = () => {
   const { user } = useAuth();
 
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <ScreenWrapper>
       <Typo>Home</Typo>
-      <Button onPress={handleLogout}>
-        <Typo color={colors.black}>Logout</Typo>
-      </Button>
     </ScreenWrapper>
   );
 };
